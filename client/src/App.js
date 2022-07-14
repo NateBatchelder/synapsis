a2673007af92a0001e3788bbc7c0a88897427943
Nathan Batchelder <natebatchelder@gmail.com>
2022-07-14 16:33:30 -0700

    adding pages to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// navigation bar
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Synapsis</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
